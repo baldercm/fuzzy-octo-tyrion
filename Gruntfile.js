c51af00fdf49358838b622f1af331cbc84f327be
Bálder Carraté <baldercm@gmail.com>
2013-10-01 17:19:22 +0200

    grunt-watch: coffee, copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  
+  "use strict";
   grunt.initConfig({
     coffee: {
       compile: {
@@ -10,26 +10,50 @@ module.exports = function(grunt) {
         ext: ".js"
       }
     },
-    requirejs: {
-      compile: {
-        options: {
-          mainConfigFile: "src/js/build.js",
-          baseUrl: "src/js",
-          name: "app",
-          out: "public/js/app-min.js"
-        }
-      }
-    },
     copy: {
       js: {
         files: [
           {expand: true, cwd: "generated", src: ["**/*.js"], dest: "public"}
         ]
+      },
+      css: {
+        files: [
+          {expand: true, cwd: "src", src: ["**/*.css"], dest: "public"}
+        ]
+      },
+      html: {
+        files: [
+          {expand: true, cwd: "src", src: ["**/*.html"], dest: "public"}
+        ]
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          mainConfigFile: "public/js/build.js",
+          baseUrl: "public/js",
+          name: "app",
+          out: "public/js/app-min.js"
+        }
+      }
+    },
+    watch: {
+      files: "src/coffee/**/*.coffee",
+      tasks: ["coffee", "copy:js"],
+      options: {
+        spawn: false,
       }
     }
+  });
+
+  grunt.event.on("watch", function(action, filepath) {
+    grunt.config("coffee.compile.cwd", ".");
+    grunt.config("coffee.compile.src", filepath);
+    grunt.config(["copy", "js"], filepath);
   });
   
   grunt.loadNpmTasks("grunt-contrib-coffee");
   grunt.loadNpmTasks("grunt-contrib-requirejs");
   grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 };
