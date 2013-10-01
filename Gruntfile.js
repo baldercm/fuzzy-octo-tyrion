c8de3e86b4a4cd3e3303fef15e0da7f54a156861
Bálder Carraté <baldercm@gmail.com>
2013-10-01 17:54:54 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,77 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
   grunt.initConfig({
     coffee: {
-      compile: {
+      build: {
         expand: true,
-        cwd: "src/coffee",
-        src: ["**/*.coffee"],
-        dest: "generated/js",
-        ext: ".js"
+        cwd: 'src/coffee',
+        src: ['**/*.coffee'],
+        dest: 'generated/js',
+        ext: '.js'
+      },
+      watch: {
+        expand: true,
+        cwd: '.',
+        dest: 'generated/js',
+        ext: '.js'
       }
     },
     copy: {
+      build: {
+        files: [
+          {expand: true, cwd: 'generated', src: ['**/*.js'], dest: 'public'},
+          {expand: true, cwd: 'src', src: ['**/*.css'], dest: 'public'},
+          {expand: true, cwd: 'src', src: ['**/*.html'], dest: 'public'}
+        ]
+      },
       js: {
         files: [
-          {expand: true, cwd: "generated", src: ["**/*.js"], dest: "public"}
+          {expand: true, cwd: 'generated', src: ['**/*.js'], dest: 'public'}
         ]
       },
       css: {
         files: [
-          {expand: true, cwd: "src", src: ["**/*.css"], dest: "public"}
+          {expand: true, cwd: 'src', src: ['**/*.css'], dest: 'public'}
         ]
       },
       html: {
         files: [
-          {expand: true, cwd: "src", src: ["**/*.html"], dest: "public"}
+          {expand: true, cwd: 'src', src: ['**/*.html'], dest: 'public'}
         ]
       }
     },
     requirejs: {
-      compile: {
+      build: {
         options: {
-          mainConfigFile: "public/js/build.js",
-          baseUrl: "public/js",
-          name: "app",
-          out: "public/js/app-min.js"
+          mainConfigFile: 'public/js/build.js',
+          baseUrl: 'public/js',
+          name: 'app',
+          out: 'public/js/app-min.js'
         }
       }
     },
     watch: {
-      files: "src/coffee/**/*.coffee",
-      tasks: ["coffee", "copy:js"],
-      options: {
-        spawn: false,
+      coffee: {
+        files: 'src/coffee/**/*.coffee',
+        tasks: ['coffee:build', 'copy:js'],
+        options: {
+          spawn: false,
+        }
       }
     },
-    clean: ["generated", "public"]
+    clean: ['generated', 'public']
   });
 
-  grunt.event.on("watch", function(action, filepath) {
-    grunt.config("coffee.compile.cwd", ".");
-    grunt.config("coffee.compile.src", filepath);
-    grunt.config(["copy", "js"], filepath);
-  });
+//   grunt.event.on('watch', function(action, filepath) {
+//     grunt.config('coffee.watch.src', filepath);
+//     grunt.config('copy.js', filepath);
+//   });
+
+  grunt.registerTask('build', ['clean', 'coffee:build', 'copy:build']);
   
-  grunt.loadNpmTasks("grunt-contrib-coffee");
-  grunt.loadNpmTasks("grunt-contrib-requirejs");
-  grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 };
